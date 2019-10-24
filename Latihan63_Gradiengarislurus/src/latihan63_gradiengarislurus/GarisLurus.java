e53fc167d5f95dd29fe6d993896ebe4b638c246b
M islahul umam djasuta <56225661+MohIslahulumam@users.noreply.github.com>
2019-10-24 20:42:34 +0700

    Add files via upload

---
+++ Latihan63_Gradiengarislurus/src/latihan63_gradiengarislurus/GarisLurus.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan63_gradiengarislurus;
+
+/**
+ *
+ * @author RYZEN
+ */
+public interface GarisLurus {
+
+    
+
+    public int hitungGradien();
+
+        
+
+    
+
+    
+
+}
